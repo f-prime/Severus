329add4e6a42faa72234a908e56a8b43bb2347cc
Frankie <frankieprimerano@gmail.com>
2018-06-28 10:19:52 -0400

    Determine if leech or relay

--- run_severus.py
+++ run_severus.py
@@ -1,8 +1,45 @@
 import severus
 import _thread as thread
+import socket
+import requests
+import random
+import threading
+import time
 
-thread.start_new_thread(severus.listen, ())
-severus.sync()
+def is_relay():
+    def listen(port):
+        s = socket.socket()
+        s.bind(("0.0.0.0", port))
+        s.listen(5)
+        _, __ = s.accept()
+        _.close()
+    port = random.randint(5000, 9999)
+    ip = requests.get("https://bot.whatismyipaddress.com").content
+    while port == severus.config.port:
+        port = random.randint(5000, 9999)
+    t = threading.Thread(target=listen, args=(port,))
+    t.start()
+    s = socket.socket()
+    try:
+        s.settimeout(5)
+        s.connect((ip, port))
+    except:
+        s.close()
+        return False
+    s.close()
+    return True
 
-while True:
-    pass
+relay = is_relay()
+
+if is_relay():
+    print("Relay Node")
+    thread.start_new_thread(severus.listen, ())
+    severus.sync()
+    while True:
+        pass
+else:
+    print("Leech node")
+    while True:
+        severus.sync()
+        time.sleep(30)
+
